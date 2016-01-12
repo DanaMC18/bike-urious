d2f207b8e8f9fe6ec4c0ca832893bfdf2bda53d5
Dana Czinsky <DanaCzinsky@gmail.com>
2016-01-12 15:30:36 -0500

    next to nothing

--- app.js
+++ app.js
@@ -19,19 +19,22 @@ MongoClient.connect(mongoUrl, function (err, database) {
     console.log(err)
   }
   console.log('Connected!');
-  db = database;
+  db = database
 
-  db.collection('bikes').insert({
-    name: 'Learn to Ride',
-    sponsor: 'Bike New York',
-    description: 'Learn to Ride-Adults is a free class for adults and mature teens who are ready to ride. Doesn\'t matter if you\'re 18 or 80, we\'ll get you rolling in no time. With our safe, easy, effective method, Learn to Ride students learn how to balance, pedal, stop and steer a bike, as well as adjust a helmet for proper fit. Most people learn to ride in one session, but even if they don’t, they’ll leave equipped with an easy, low-stress way to teach themselves—or, they can join us for another free class!',
-    max_attendees: 25,
-    rsvps: [],
-    comments: [{
-      name: 'Anna',
-      comment: 'Sounds like a great time!'
-    }]
-  })
+  // db.collection('bikes').remove({});
+
+  // db.collection('bikes').insert({
+  //   title: 'Learn to Ride',
+  //   sponsor: 'Bike New York',
+  //   start: '2016-01-12',
+  //   description: 'Learn to Ride-Adults is a free class for adults and mature teens who are ready to ride. Doesn\'t matter if you\'re 18 or 80, we\'ll get you rolling in no time. With our safe, easy, effective method, Learn to Ride students learn how to balance, pedal, stop and steer a bike, as well as adjust a helmet for proper fit. Most people learn to ride in one session, but even if they don’t, they’ll leave equipped with an easy, low-stress way to teach themselves—or, they can join us for another free class!',
+  //   max_attendees: 25,
+  //   rsvps: [],
+  //   comments: [{
+  //     name: 'Anna',
+  //     comment: 'Sounds like a great time!'
+  //   }]
+  // })
 
 
   process.on('exit', db.close);
@@ -41,9 +44,22 @@ app.get('/', function (req, res){
   res.render('index');
 })
 
-app.get('/events', function (req, res){
-  res.render('events');
+app.get('/calendars', function (req, res){
+  db.collection('bikes').find({}).toArray(function (err, results){
+    console.log(results)
+    res.render('events', {events: results});     
+  })
 })
 
+app.get('/events', function (req, res){
+  // db.collection('bikes').findOne({start: })
+})
 
-app.listen(3000)
+app.listen(3000);
+
+
+
+
+
+
+
--- public/main.js
+++ public/main.js
@@ -1,6 +1,25 @@
-console.log('bike!')
+console.log('bicycle! bicycle!')
 
 $(document).ready(function(){
+ 
+ $('#calendar').fullCalendar({
+    // var events = require('../lib/events.json')
+    dayClick: function(){
+      console.log('a day has been clicked!!')
+      // $.ajax({
+      //   url: '/days_events',
+      //   type: 'get',
+      //   dataType: 'json',
+      //   data: 
+      //   }).done(function(response){
+      //     console.log(response);
+      //   })
+    }
+ })
 
-  
+ $('#calendar').fullCalendar('next');
+
+
+
+
 })
